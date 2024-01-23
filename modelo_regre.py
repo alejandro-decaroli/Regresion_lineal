50e2fbee2c1c3a5546f7dff80bbe5b157131898a
Alejandro Decaroli <alejandro.decaroli.97@gmail.com>
2024-01-23 20:45:16 -0300

    Eliminacion de librerias innecesarias

--- modelo_regre.py
+++ modelo_regre.py
@@ -1,11 +1,8 @@
 ## Libraries
 
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.linear_model import LinearRegression
 
 ## Dataset Analysis
